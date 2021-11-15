6cc5a59c01bcfd419e9b04ee68a99ff3d599ea21
grubi97 <grubesa.marko97@gmail.com>
2021-11-15 20:35:01 +0100

    again

--- Source/Break/Break.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "Break.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Break, "Break" );
